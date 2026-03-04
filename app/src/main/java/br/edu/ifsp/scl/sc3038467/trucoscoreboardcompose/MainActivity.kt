8232b91a83ee663e067036e34830021a9e87e0dc
MuriloCSS <murilosao06@gmail.com>
2026-03-04 15:09:30 -0300

    feat: cria MainActivity com setup do Compose e  adiciona estados do jogo no TrucoApp

--- app/src/main/java/br/edu/ifsp/scl/sc3038467/trucoscoreboardcompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038467/trucoscoreboardcompose/MainActivity.kt
@@ -3,27 +3,28 @@ package br.edu.ifsp.scl.sc3038467.trucoscoreboardcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.sc3038467.trucoscoreboardcompose.ui.theme.TrucoScoreBoardComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TrucoScoreBoardComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    TrucoApp()
                 }
             }
         }
@@ -31,17 +32,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TrucoApp() {
+    var scoreA by remember { mutableIntStateOf(0) }
+    var scoreB by remember { mutableIntStateOf(0) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrucoScoreBoardComposeTheme {
-        Greeting("Android")
-    }
+    var alertMao11ShownA by remember { mutableStateOf(false) }
+    var alertMao11ShownB by remember { mutableStateOf(false) }
+
+    var showDialog by remember { mutableStateOf(false) }
+    var dialogTitle by remember { mutableStateOf("") }
+    var dialogMessage by remember { mutableStateOf("") }
+    var isGameOver by remember { mutableStateOf(false) }
+
 }
