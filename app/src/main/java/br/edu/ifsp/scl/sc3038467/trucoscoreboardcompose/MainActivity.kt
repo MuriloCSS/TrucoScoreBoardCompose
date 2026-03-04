22b3d66ca45108b6982bac314e1616f9e5eb255d
MuriloCSS <murilosao06@gmail.com>
2026-03-04 15:45:10 -0300

    feat(score): implementa lógica de pontuação e verificação de regras

--- app/src/main/java/br/edu/ifsp/scl/sc3038467/trucoscoreboardcompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038467/trucoscoreboardcompose/MainActivity.kt
@@ -53,4 +53,38 @@ fun TrucoApp() {
         isGameOver = false
         showDialog = false
     }
+
+    fun addPoints(isTeamA: Boolean, points: Int) {
+        if (isTeamA) scoreA += points else scoreB += points
+
+        if (scoreA >= 12) {
+            dialogTitle = "Fim de Jogo"
+            dialogMessage = "A Equipe A venceu a partida!"
+            isGameOver = true
+            showDialog = true
+        } else if (scoreB >= 12) {
+            dialogTitle = "Fim de Jogo"
+            dialogMessage = "A Equipe B venceu a partida!"
+            isGameOver = true
+            showDialog = true
+        } else {
+            if (scoreA == 11 && scoreB == 11 && (!alertMao11ShownA || !alertMao11ShownB)) {
+                alertMao11ShownA = true
+                alertMao11ShownB = true
+                dialogTitle = "Atenção: Mão de 11!"
+                dialogMessage = "Ambas as equipes estão com 11 pontos"
+                showDialog = true
+            } else if (scoreA == 11 && !alertMao11ShownA) {
+                alertMao11ShownA = true
+                dialogTitle = "Atenção: Mão de 11!"
+                dialogMessage = "A Equipe A está na mão de 11!"
+                showDialog = true
+            } else if (scoreB == 11 && !alertMao11ShownB) {
+                alertMao11ShownB = true
+                dialogTitle = "Atenção: Mão de 11!"
+                dialogMessage = "A Equipe B está na mão de 11!"
+                showDialog = true
+            }
+        }
+    }
 }
