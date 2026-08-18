5723375bd8b58909e623d7c8965dd42f146039fd
Murilo Corrêa <murilosao06@gmail.com>
2026-08-18 13:30:50 -0300

    feat: add estrutura base da tela das equipes

--- app/src/main/java/br/edu/ifsp/scl/sc3038467/trucoscoreboardcompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038467/trucoscoreboardcompose/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.sc3038467.trucoscoreboardcompose.ui.theme.TrucoScoreBoardComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrucoScoreBoardComposeTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
+                    ContadorTruco(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +36,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContadorTruco(name: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        ) {
+            Text(text = "Equipe A", fontSize = 32.sp, modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 64.dp)
+
+            )
+
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        ) {
+            Text(text = "Equipe B", fontSize = 32.sp, modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 64.dp)
+
+            )
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ContadorTrucoPreview() {
     TrucoScoreBoardComposeTheme {
-        Greeting("Android")
+        ContadorTruco("Android")
     }
 }
